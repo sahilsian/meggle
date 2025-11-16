fc0511c3f98852b8685724d8d38780653a957a11
Sahil Sian <sh864246@dal.ca>
2025-11-15 20:25:00 -0400

    migrating to other laptop

---
+++ CommunityService/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.AddServiceDefaults();
+builder.Services.AddAuthentication()
+    .AddKeycloakJwtBearer(serviceName: "keycloak", realm: "meggle", options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.Audience = "meggle";
+    });
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapDefaultEndpoints();
+
+app.Run();
--- Meggle.AppHost/AppHost.cs
+++ Meggle.AppHost/AppHost.cs
@@ -1,6 +1,22 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var keycloak = builder.AddKeycloak("keycloak", 6001)
     .WithDataVolume("keycloak-data");
+
+var postgres = builder.AddPostgres("postgres", port: 5432)
+    .WithDataVolume("postgres-data")
+    .WithPgAdmin();
+
+var communityDb = postgres.AddDatabase("community-db");
+
+
+var communityService = builder.AddProject<CommunityService>("community-svc")
+    .WithReference(keycloak)
+    .WithReference(communityDb)
+    .WaitFor(keycloak)
+    .WaitFor(communityDb);
+
 
 builder.Build().Run();
